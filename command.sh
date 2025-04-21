79dfc9edefe5cb85fff8a8fc2e7fa3c2f92c59fc
Suresh G Kini <sureshgkini.ece@gmail.com>
2025-04-21 19:27:53 +0530

    Add command.sh

---
+++ command.sh
@@ -0,0 +1,5 @@
+
+# perl command to replace one keyword with another 
+perl -i -pe 's/\.A\(/\.AN\(/g if /NAND2BX4/' ../inputs/rtl/vorca_fixed.v
+
+
--- tcl_scripts/vorca_run.tcl
+++ tcl_scripts/vorca_run.tcl
@@ -1,5 +1,5 @@
 read_lib /jk/Genus/Genus_CUI_RAK-20250130T063649Z-001/Genus_CUI_RAK/LIB/slow.lib
-read_verilog /home/01fe21bec255/DFT_Internship/inputs/rtl/vorca.v
+read_verilog /home/01fe21bec255/DFT_Internship/inputs/rtl/vorca_fixed.v
 set load_netlist_ignore_undefined_cell true
 set_top_module
 read_sdc /home/01fe21bec255/DFT_Internship/inputs/sdc/vorca_constraints.sdc
