79e1157d6c53383d1bd2d9b2a185ed4f02df45ce
Suresh G Kini <97239136+sureshkini09@users.noreply.github.com>
2025-09-26 15:05:13 +0530

    Update command.sh
    perl command to ground SE and Si

--- command.sh
+++ command.sh
@@ -2,4 +2,8 @@
 # perl command to replace one keyword with another 
 perl -i -pe 's/\.A\(/\.AN\(/g if /NAND2BX4/' ../inputs/rtl/vorca_fixed.v
 
+#perl command to ground SI and SE pins
+perl -0777 -pe "s/\.SE\s*\(\s*.*?\s*\)/.SE(1'b0)/gs; s/\.SI\s*\(\s*.*?\s*\)/.SI(1'b0)/gs;" cpu_sys.v > cpu_sys_modified.v
+
+
 
